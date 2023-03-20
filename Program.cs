02845045815f549de5fb6bbbf3dc9f16352f3aab
lokhno <x_hunter@mail.ru>
2023-03-20 12:51:23 +0300

    Неудачная попытка

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите 5_значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int [] array = {0, 0, 0, 0, 0};  
+int work_number = number;
+int i; int j;
+if (number > 9999 && number < 100000)
+  {
+    for (i = 0 ; i < array.Length ; i ++);
+    array [i] = work_number % 10;
+    work_number = work_number / 10;
+  }
+else Console.WriteLine("Incorrect number");
+if (array[1] == array[5] && array[2] == array[4]) Console.WriteLine("Полиндром");
+
+for (j = 0 ; j < array.Length ; j ++);
+    Console.WriteLine(array [j]);
